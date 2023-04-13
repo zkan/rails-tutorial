c7dcc9788b3aadf8013e3c78af0da97a4bf659d5
zkan <zkan.cs@gmail.com>
2023-04-13 08:17:32 +0700

    feat: Create microposts

--- toy_app/config/routes.rb
+++ toy_app/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'application#hello'
+  # root 'application#hello'
+  root 'users#index'
 end
