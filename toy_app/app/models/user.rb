d5ca571b60a10e47c69d324effd262405aedccb5
zkan <zkan.cs@gmail.com>
2023-12-10 20:14:06 +0700

    Add validation on name format and email confirmation

--- toy_app/app/models/user.rb
+++ toy_app/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts
   validates :name, presence: true
-  validates :email, presence: true
+  validates :name, format: { with: /\A[a-zA-Z]+\z/ },
+    length: { minimum: 3, maximum: 50 }
+  validates :email, presence: true, confirmation: true
 end
