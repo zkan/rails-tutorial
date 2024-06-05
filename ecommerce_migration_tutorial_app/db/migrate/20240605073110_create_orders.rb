7af2188b7778d4a9900235419cb613cc41dd2142
zkan <zkan.cs@gmail.com>
2024-06-05 14:31:14 +0700

    Create order model that links to buyer

---
+++ ecommerce_migration_tutorial_app/db/migrate/20240605073110_create_orders.rb
@@ -0,0 +1,9 @@
+class CreateOrders < ActiveRecord::Migration[7.1]
+  def change
+    create_table :orders do |t|
+      t.references :buyer, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
