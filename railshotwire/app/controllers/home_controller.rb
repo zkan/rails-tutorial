d1857d249aa87f0702514c1d15a6fc9c35663035
zkan <zkan.cs@gmail.com>
2024-06-09 19:38:44 +0700

    Play with turbo frame

--- railshotwire/app/controllers/home_controller.rb
+++ railshotwire/app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+
+  # @route GET /turbo_frame_form
+  def turbo_frame_form
+  end
+
+  # @route POST /turbo_frame_submit
+  def turbo_frame_submit
+    extracted_anynumber = params[:any][:anynumber]
+    render :turbo_frame_form, status: :ok, locals: {anynumber: extracted_anynumber, comment: 'turbo_frame_submit ok' }
+  end
 end
--- railshotwire/config/routes.rb
+++ railshotwire/config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  get '/home/turbo_frame_form' => 'home#turbo_frame_form', as: 'turbo_frame_form'
+  post '/home/turbo_frame_submit' => 'home#turbo_frame_submit', as: 'turbo_frame_submit'
+
 end
