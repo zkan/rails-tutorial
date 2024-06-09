9a6c4548d0f56036c621d9964d4d15a433c71093
zkan <zkan.cs@gmail.com>
2024-06-09 20:14:16 +0700

    Try turbo stream

--- railshotwire/config/routes.rb
+++ railshotwire/config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   get '/home/turbo_frame_form' => 'home#turbo_frame_form', as: 'turbo_frame_form'
   post '/home/turbo_frame_submit' => 'home#turbo_frame_submit', as: 'turbo_frame_submit'
 
+  post '/other/post_something' => 'other#post_something', as: 'post_something'
+
 end
