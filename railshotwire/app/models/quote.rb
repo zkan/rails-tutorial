4933e015caa20555da3895bd24ff19033dbc961d
zkan <zkan.cs@gmail.com>
2024-06-11 06:17:25 +0700

    Broadcast after commit

--- railshotwire/app/models/quote.rb
+++ railshotwire/app/models/quote.rb
@@ -1,3 +1,11 @@
 class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit :broadcast_prepend_to_quotes
+  def broadcast_prepend_to_quotes
+    broadcast_prepend_to(
+      :broadcast_quotes,
+      target: "quotes"
+    )
+  end
 end
