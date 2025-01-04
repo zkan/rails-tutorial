fce08c4beca82a7622d3e291a011f97ac80fc474
zkan <zkan.cs@gmail.com>
2025-01-04 10:48:22 +0700

    Add products controller index

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -11,4 +11,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # get "/products", to: "products#index"
+  #
+  # get "/products/new", to: "products#new"
+  # post "/products", to: "products#create"
+  #
+  # get "/products/:id", to: "products#show"
+  #
+  # get "/products/:id/edit", to: "products#edit"
+  # patch "/products/:id", to: "products#update"
+  # put "/products/:id", to: "products#update"
+  #
+  # delete "/products/:id", to: "products#destroy"
+
+  resources :products
+
 end
