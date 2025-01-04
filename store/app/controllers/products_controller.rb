084890df603b632fb3b84806577bba57f863d8ea
zkan <zkan.cs@gmail.com>
2025-01-04 17:40:21 +0700

    Edit product

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update ]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -20,7 +21,23 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
   private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
     def product_params
       params.expect(product: [ :name ])
     end
