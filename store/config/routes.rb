a600327c1930dacbb04fbeb7633144d37dc90b63
zkan <zkan.cs@gmail.com>
2025-01-04 16:49:53 +0700

    Show individual product page

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "products#index"
 
   # get "/products", to: "products#index"
   #
