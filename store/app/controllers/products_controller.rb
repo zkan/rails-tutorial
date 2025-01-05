6598e0a48c10a32536b6b320af4ebc340514e9c3
zkan <zkan.cs@gmail.com>
2025-01-05 08:21:49 +0700

    Add logout and allow unauthenticated access for index and show

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  allow_unauthenticated_access only: %i[ index show ]
   before_action :set_product, only: %i[ show edit update destroy ]
 
   def index
