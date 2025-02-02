db7a58bc388d75cbbd0cfb950a26a763172a3f21
zkan <zkan.cs@gmail.com>
2025-02-02 13:59:06 +0700

    Add tests for product notification

--- store/test/mailers/product_mailer_test.rb
+++ store/test/mailers/product_mailer_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 class ProductMailerTest < ActionMailer::TestCase
   test "in_stock" do
-    mail = ProductMailer.in_stock
+    mail = ProductMailer.with(product: products(:tshirt), subscriber: subscribers(:david)).in_stock
     assert_equal "In stock", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
+    assert_equal [ "david@example.org" ], mail.to
     assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "Good news!", mail.body.encoded
   end
 end
--- store/test/models/product_test.rb
+++ store/test/models/product_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include ActionMailer::TestHelper
+
+  test "sends email notifications when back in stock" do
+    product = products(:tshirt)
+
+    # Set product out of stock
+    product.update(inventory_count: 0)
+
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
 end
