c958752e984f1c6e06c5addc29d58728b3864a3e
zkan <zkan.cs@gmail.com>
2025-02-02 10:49:20 +0700

    Set i18n for Thai

--- store/app/controllers/application_controller.rb
+++ store/app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 end
